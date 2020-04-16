0d310c8f2061d09f383dc757b70eaa7531033928
tanya <shanya.2018td@gmail.com>
2020-04-16 16:23:34 +0530

    auto keyword use

---
+++ algorithm_header/auto_use.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int a=45;
+	auto b=a;
+	auto c=65;
+	auto fl=9.9;
+	auto gl="tanya";
+	cout<<b<<" "<<c<<" "<<fl<<" "<<gl;
+	return 0;
+}
