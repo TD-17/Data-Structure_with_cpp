a80f361b795efdc7f82b22642a50b8c7ea763038
tanya <shanya.2018td@gmail.com>
2020-04-20 20:39:44 +0530

    added accumulate function for string

--- algorithm_header/accumulate.cpp
+++ algorithm_header/accumulate.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	vector<int> a{2,3,4,5,6};
+	vector<string> words{"one","two","three"};
 	//sum of elements in vector a.
 	int total=accumulate(begin(a),end(a),0);
 	cout<<total<<endl;
@@ -12,5 +13,11 @@ int main()
 	total=accumulate(begin(a),end(a),0,[](int total,int i){if(i%2==0) return total+i; return total;
 	});
 	cout<<total<<endl;
+	//accumulate function on vector words of string
+	auto allwords=accumulate(begin(words),end(words),string{});
+	cout<<allwords<<endl;
+	allwords=accumulate(begin(words),end(words),string{"words:"},[](const string &total,string &s){return total+" "+s;
+	});
+	cout<<allwords<<endl;
 	return 0;
 }
