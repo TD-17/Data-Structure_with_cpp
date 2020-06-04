fc5a78d0848fda72f8007a163ad33eb856ff5bbe
tanya <shanya.2018td@gmail.com>
2020-06-04 21:21:58 +0530

    insertion in map

---
+++ Map/mapInsertion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iterator>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+	map<int,int> numbers;
+	map<int,int>::iterator it;
+	
+//	entering values in map numbers
+	numbers.insert(pair<int, int>(1,40));
+	numbers.insert(pair<int, int>(2,23));
+	numbers.insert(pair<int, int>(3,56));
+	numbers.insert(pair<int, int>(4,12));
+	
+//	printing map numbers
+	for(it=numbers.begin();it!=numbers.end();it++)
+	{
+			cout<<"first value is "<<it->first;
+	        cout<<"  second value is "<<it->second;
+	        cout<<endl;
+	}
+
+	return 0;
+}
