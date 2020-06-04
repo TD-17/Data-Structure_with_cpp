ecfb29221a57c7ee31f1b870bc44d7f30c54f411
tanya <shanya.2018td@gmail.com>
2020-06-05 02:25:14 +0530

    insertion in multimap

---
+++ Multimap/insertMultimap.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iterator>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+	multimap<int,int> multimp;
+	multimp.insert(pair<int,int>(1,45));
+	multimp.insert(pair<int,int>(2,54));
+	multimp.insert(pair<int,int>(2,44));
+	multimp.insert(pair<int,int>(3,54));
+	multimp.insert(pair<int,int>(4,50));
+	
+	multimap<int,int>::iterator it;
+	for(it=multimp.begin();it!=multimp.end();it++)
+	{
+		cout<<"Key "<<it->first;
+		cout<<" Value "<<it->second<<endl;
+	}
+	return 0;
+}
